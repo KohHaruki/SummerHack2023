014668f610b547e2cb6e12d89b8bcc67c68967fe
HARUKI KOH <harukikoh@HARUKIs-MacBook-Pro-M1-Pro.local>
2023-02-05 21:06:52 +1100

    Implemented React Router, and made minor CSS adjustment for anchor tags

---
+++ client/src/layouts/Header.tsx
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom'
+import logo from '../assets/react.svg'
+import './Header.css'
+
+const Header = () => {
+    
+    return (
+        <header>
+            <nav className="nav">
+                <img className="logo" src={logo} />
+                <ul className="nav">
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="about">About</NavLink></li>
+                    <li><NavLink to="developer">For Developers</NavLink></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
