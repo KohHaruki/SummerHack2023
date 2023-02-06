de0e31da8e18d6b7c3d76323f0c7aeae079d10f0
GrassyAirplane <euanlimcs@gmail.com>
2023-02-06 23:23:15 +0800

    server additions

--- server/server.js
+++ server/server.js
@@ -1,36 +1,42 @@
 const app = require('express')()
+const cors = require('cors')
+
+const multer = require('multer');
+
+const fileStorageEngine = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "../scanner/image_to_scan")
+  },
+  filename: (req, file, cb) => {
+    cb(null, "image.png")
+  }
+})
+
+const upload = multer({ storage: fileStorageEngine})
 
 // Code for python
 const { spawn } = require('child_process');
-const fs = require('fs');
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({origin:"http://127.0.0.1:5173"}));
 
 app.get('/home', (req,res)=> {
   res.end("HELLO THERE")  
 })
-app.post('/api/uploadfile', (req, res)=> {
+
+app.post('/api/uploadfile', upload.single('image'), (req, res)=> {
   res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-  // TEST
-  const formData = req.body;
-  //const image = formData.get(`file_0`); EXTRA
-  console.log(req)
-
-  const pathToImage = '../scanner/image_to_scan/image.png';
-  console.log(typeof(image))
-  // fs.writeFileSync(pathToImage, image);
-  
-  const pythonProcess = spawn('python3', ["../scanner/pipeline.py", pathToImage]);
+  res.header("Access-Control-Allow-Methods", "PUT POST")
+    
+  const pythonPath = "../scanner/pipeline.py"
+  const pythonProcess = spawn('python3', [pythonPath]);
   pythonProcess.stdout.on('data', (data) => {
     // Handle the data returned from the Python function
     const result = JSON.parse(data.toString());
     res.json(result);
   });
-  })
+})
 
 app.listen(5000, ()=> {
     console.log("Express app listening")
