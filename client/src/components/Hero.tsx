92ba5dd32f0ecdd626d3ac3c66eefc65fd704b91
NiftyCoffee <zoetayss@gmail.com>
2023-02-09 22:12:14 +0800

    Changed layout and added a blank login feature

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,6 +1,6 @@
 const Hero = (props: any) => {
     return (
-        <div className="hero">
+        <div className="hero" id={props.id}>
             <h2 className="hero-title">{props.title}</h2>
             <h3 className="hero-subtitle">{props.subtitle}</h3>
         </div>
