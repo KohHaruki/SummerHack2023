6c0a320483900ae114cf594881a6a317ff84809a
Haruki Koh <harukikoh@gmail.com>
2023-02-19 19:37:05 +1100

    deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/SummerHack2023',
+  base: "/SummerHack2023",
   plugins: [react()],
 })
